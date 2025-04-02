631c3f9936dd406a73f1eefb7b08cfc25cfc89f5
ajiaja38 <ajicooljazz38@gmail.com>
2025-04-02 20:02:34 +0700

    feat: update minor

--- schema/deviceSchema.ts
+++ schema/deviceSchema.ts
@@ -1,5 +1,5 @@
-import { EDevice } from "@/types/enum/EDevice.enum";
-import { z } from "zod";
+import { EDevice } from "@/types/enum/EDevice.enum"
+import { z } from "zod"
 
 export const registerDeviceSchema = z.object({
   guid: z
@@ -9,7 +9,7 @@ export const registerDeviceSchema = z.object({
     .string({ required_error: "Mac Address is required" })
     .min(1, { message: "Mac Address is required" }),
   type: z.enum([EDevice.AKTUATOR, EDevice.SENSOR], {
-    message: "Type is required",
+    message: "Deivce Type is required",
   }),
   quantity: z.string().min(1, { message: "Quantity is required" }),
   name: z
@@ -21,4 +21,4 @@ export const registerDeviceSchema = z.object({
   minor: z
     .string({ required_error: "Minor is required" })
     .min(1, { message: "Minor is required" }),
-});
+})
