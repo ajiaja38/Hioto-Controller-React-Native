ea02978c2cee42559238ae2087c226fc5c2829a7
ajiaja38 <ajicooljazz38@gmail.com>
2025-04-04 21:38:22 +0700

    feat: update minor

--- schema/deviceSchema.ts
+++ schema/deviceSchema.ts
@@ -9,7 +9,7 @@ export const registerDeviceSchema = z.object({
     .string({ required_error: "Mac Address is required" })
     .min(1, { message: "Mac Address is required" }),
   type: z.enum([EDevice.AKTUATOR, EDevice.SENSOR], {
-    message: "Deivce Type is required",
+    message: "Deivce is required",
   }),
   quantity: z.string().min(1, { message: "Quantity is required" }),
   name: z
