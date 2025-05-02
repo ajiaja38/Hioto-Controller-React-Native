98f72b23d2540116492177a15ffc06ef43572511
ajiaja38 <ajicooljazz38@gmail.com>
2025-05-02 08:38:07 +0700

    feat: update dikit aje :art:

--- schema/ruleSchema.ts
+++ schema/ruleSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const createRuleSchema = z.object({
   inputDevice: z
-    .string({ required_error: "Input Device is required, choose at least one" })
+    .string({ required_error: "Input Device is required" })
     .min(1, "Input Device is required"),
   outputDevices: z
     .array(z.object({ value: z.string() }))
