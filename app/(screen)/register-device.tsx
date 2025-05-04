79c8047a724cb005bc11332863b27339ccf5065d
ajiaja38 <ajicooljazz38@gmail.com>
2025-05-04 11:41:02 +0700

    feat: update minor

--- app/(screen)/register-device.tsx
+++ app/(screen)/register-device.tsx
@@ -28,8 +28,7 @@ import { Dropdown } from "react-native-element-dropdown"
 type RegisterDeviceSchema = z.infer<typeof registerDeviceSchema>
 
 const RegisterDevice: React.FC = (): React.JSX.Element => {
-  const [loading, setLoading]: [boolean, (value: boolean) => void] =
-    useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
   const { toastSuccess, toasError } = useToast()
 
   const router: Router = useRouter()
